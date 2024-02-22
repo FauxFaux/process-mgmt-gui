b2d94d9dcfd2860a573a586f098c5be40762a9db
Chris West (Faux) <git@goeswhere.com>
2024-02-22 15:19:29 +0000

    chore: hello world

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import path from 'node:path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __dirname = import.meta.dirname;
+if (!__dirname) {
+  throw new Error('requires node >20.11 for no particular reason');
+}
+
+export default {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'static/bundle.[contenthash].js',
+    library: 'app',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: 'source-map',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [{ loader: 'babel-loader' }],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
